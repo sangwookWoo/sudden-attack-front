f2196673892985f4bd44d33b6d23f314c4cb49d5
woody <woody@ip-172-30-1-94.ap-northeast-2.compute.internal>
2024-02-09 03:07:05 +0900

    [feat] add footer

---
+++ home.js
@@ -0,0 +1,20 @@
+const swiperEl = document.querySelector('swiper-container');
+const buttonEl = document.querySelector('button');
+
+buttonEl.addEventListener('click', () => {
+    swiperEl.swiper.slideNext();
+  });
+
+const images = [
+    "https://cdn.pixabay.com/photo/2024/01/08/15/54/defile-8495836_1280.jpg", 
+    "https://cdn.pixabay.com/photo/2024/01/27/18/24/squirrel-8536537_1280.jpg", 
+    "https://cdn.pixabay.com/photo/2024/01/16/21/25/copenhagen-8513129_1280.jpg"];
+
+// // 이미지 슬라이더에 이미지 추가
+// const slider = document.getElementById('image-slider');
+// images.forEach(image => {
+//     const slide = document.createElement('swiper-slide');
+//     // slide.className = 'slide';
+//     slide.innerHTML = `<img src="${image}" alt="이미지" class="w-full h-auto">`;
+//     slider.appendChild(slide);
+// });
